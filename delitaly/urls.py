39145af9fa9fe67900567de3701eb43f407faef7
Renato Bianco <renato_bianco@apple.com>
2020-08-15 17:12:27 +0100

    added footer to  base.html, completed index.html, contactus.html, privacy.html, sent.html, shipping.html

--- delitaly/urls.py
+++ delitaly/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('', include('home.urls')),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    """ A view to return the index page """
-    
+def index(request): 
     return render(request, 'home/index.html')
