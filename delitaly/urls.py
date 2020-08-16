68dd5783619d962835c62a1dc244731ba3992e95
Renato Bianco <renato_bianco@apple.com>
2020-08-16 01:28:53 +0100

    added product views and templates

--- delitaly/urls.py
+++ delitaly/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),   
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ products/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products'),
+]
