3f90b87c77d1bbc70639f03f4e45df2bfc15a6cb
Renato Bianco <renato_bianco@apple.com>
2020-08-20 18:03:19 +0100

    added quantity input button -/+

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,13 +10,13 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
+        total += item_data * product.price
+        product_count += item_data
         bag_items.append({
             'item_id': item_id,
-            'quantity': quantity,
+            'quantity': item_data,
             'product': product,
         })
 
