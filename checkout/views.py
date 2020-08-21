6d6b4db126772fb65763c27d39d56d97aa09847b
Renato Bianco <renato_bianco@apple.com>
2020-08-21 19:41:13 +0100

    Added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_TYooMQauvdEDq54NiTphI7jx',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
