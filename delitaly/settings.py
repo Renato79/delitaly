c0241e4617d22cba5c2d5a3ac698a5c772388c85
Renato Bianco <renato_bianco@apple.com>
2020-08-23 20:51:48 +0100

    Removed secret key and set debug

--- delitaly/settings.py
+++ delitaly/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q&$a29)2(-7*z&t(=h6f4-yc1swboas#efo5m!70#j3qpl_x#s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['delitaly.herokuapp.com', 'localhost']
 
