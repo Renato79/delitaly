d4c9a2e4b3c04c0f4c9d5f0100a91d732a66269e
Renato Bianco <renato_bianco@apple.com>
2020-08-23 20:40:22 +0100

    Deployment to Heroku part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn delitaly.wsgi:application
--- delitaly/settings.py
+++ delitaly/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'q&$a29)2(-7*z&t(=h6f4-yc1swboas#efo5m!70#j3qpl_x#s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['delitaly.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'delitaly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
